905de01e2eb89d010dc3f70f94de52ee1e3248e4
bDino <buskulic.dino@gmail.com>
2014-03-27 22:18:23 +0100

    nichts getestet

--- beziehungen.pl
+++ beziehungen.pl
@@ -1,6 +1,6 @@
 % Implemtieren von Verwandschaftsbeziehungen
 % Alle Teilnehmer: Erste Hierarchie
-
+[load].
 weiblich(susi).
 weiblich(hannelore).
 weiblich(siglinde).
@@ -8,18 +8,27 @@ maennlich(stefan).
 maennlich(hans).
 maennlich(olaf).
 
-mutter(siglinde).
-mutter(hannelore).
-vater(olaf).
-vater(hans).
-opa(hans).
+mutter(siglinde,susi).																			% Siglinde ist Susis Mutter
+mutter(siglinde,stefan).																		% Siglinde ist Stefans Mutter
+mutter(siglinde,johanna). 																		% Sigline ist Johanns Mutter
+mutter(hannelore,siglinde).																		% Hannelore ist Siglindes Mutter
+mutter(hannelore,stefi).																		% Hannelore ist Stefis Mutter
+vater(olaf,susi). 																				% Olaf ist Susis Vater
+vater(olaf,sabrina).																			% Olaf ist Sabrinas Vater
+vater(olaf,stefan). 																			% Olaf ist Stefans Vater
+vater(hans,olaf). 																				% Hans ist Olafs Vater
+vater(hans,stefi).																				% Hans ist Stefis Vater	
+ehemann(rolf,sabrina).																			% Rolf ist Sabrinas Mann
 
-% Beziehungen
+% Beziehungen exemplarisch
 
-eltern(X,Y):- vater(Y);mutter(Y). 																				% Y ist Elternteil von X
-opa(X,Y):- maennlich(Y),eltern(eltern(X,hannelore),Y);eltern(eltern(X,olaf),Y).									% Y ist Opa von X
-geschwister(X,Y):- eltern(X,hannelore),eltern(X,olaf),eltern(Y,olaf),eltern(Y,hannelore).						% Y und X geschwister
-halb_schwester(X,Y):- weiblich(Y),eltern(X,hannelore),eltern(Y,hannelore);eltern(X,olaf),eltern(Y,olaf).		% Y ist halbschwester von				
-vollschwester(X,Y):- weiblich(Y),eltern(X,hannelore),eltern(Y,hannelore);eltern(X,olaf),eltern(Y,olaf).			% Y ist schwester von				
-tante(X,Y):- weiblich(Y),geschwister(eltern(X,hannelore));(geschwister(eltern(X,hans))).						% Y ist Tante von X
-onkel(X,Y):- maennlich(Y),geschwister(eltern(X,hannelore));(geschwister(eltern(X,hans))).						% Y ist Onkel von X
+eltern(X,Y):- vater(X,Y);mutter(X,Y). 															% X ist Elternteil von Y
+opa(X,Y):- vater(X,olaf).																		% X ist Opa von Y
+oma(X,Y):- mutter(X,siglinde).																	% X ist Opa von Y
+geschwister(X,Y):- vater(Z,X),vater(Z,Y),mutter(Z,X),mutter(Z,Y).								% X und Y geschwister
+halb_schwester(X,Y):- weiblich(X),vater(Z,X),vater(Z,Y);weiblich(X),mutter(Z,X),mutter(Z,Y).	% X ist halbschwester von				
+schwester(X,Y):- weiblich(X),geschwister(X,Y).													% X ist schwester von				
+tante(X,Y):- weiblich(X),geschwister(eltern(Y,Z));(geschwister(eltern(Y,Z))).					% X ist Tante von Y
+onkel(X,Y):- maennlich(X),geschwister(eltern(Y,Z));(geschwister(eltern(Y,Z))).					% X ist Onkel von Y
+cousin(X,Y):- geschwister(eltern(Y,Z),eltern(X,Z)). 											% X ist Cousin von Y
+schwager(X,Y):- geschwister(Z,Y),ehemann(X,Z).													% X ist Schwager von Y
