8c8371858d7d0420472bccb52d5c18561a464b9d
bDino <buskulic.dino@gmail.com>
2014-04-01 17:40:52 +0200

    verschachtelungen aus den Klauseln entfernt

--- beziehungen.pl
+++ beziehungen.pl
@@ -8,27 +8,39 @@ maennlich(stefan).
 maennlich(hans).
 maennlich(olaf).
 
-mutter(siglinde,susi).																			% Siglinde ist Susis Mutter
-mutter(siglinde,stefan).																		% Siglinde ist Stefans Mutter
-mutter(siglinde,johanna). 																		% Sigline ist Johanns Mutter
-mutter(hannelore,siglinde).																		% Hannelore ist Siglindes Mutter
-mutter(hannelore,stefi).																		% Hannelore ist Stefis Mutter
-vater(olaf,susi). 																				% Olaf ist Susis Vater
-vater(olaf,sabrina).																			% Olaf ist Sabrinas Vater
-vater(olaf,stefan). 																			% Olaf ist Stefans Vater
-vater(hans,olaf). 																				% Hans ist Olafs Vater
-vater(hans,stefi).																				% Hans ist Stefis Vater	
-ehemann(rolf,sabrina).																			% Rolf ist Sabrinas Mann
+% X ist von Y Beziehung (mutter(X,Y))
+mutter(siglinde,susi).																			
+mutter(siglinde,stefan).
+mutter(siglinde,johanna).
+mutter(hannelore,siglinde).																		
+mutter(hannelore,stefi).																		
+vater(olaf,susi). 																				
+vater(olaf,sabrina).																			
+vater(olaf,stefan). 																			
+vater(hans,olaf). 																				
+vater(hans,stefi).																				
+ehemann(rolf,sabrina).																			
 
 % Beziehungen exemplarisch
-
-eltern(X,Y):- vater(X,Y);mutter(X,Y). 															% X ist Elternteil von Y				
-opa(X,Y):- vater(X,Z),vater(Z,Y).																			% X ist Opa von Y
-oma(X,Y):- mutter(X,siglinde).																	% X ist Opa von Y
-geschwister(X,Y):- vater(Z,X),vater(Z,Y),mutter(Z,X),mutter(Z,Y).								% X und Y geschwister
-halb_schwester(X,Y):- weiblich(X),vater(Z,X),vater(Z,Y);weiblich(X),mutter(Z,X),mutter(Z,Y).	% X ist halbschwester von				
-schwester(X,Y):- weiblich(X),geschwister(X,Y).													% X ist schwester von				
-tante(X,Y):- weiblich(X),geschwister(eltern(Y,Z));(geschwister(eltern(Y,Z))).					% X ist Tante von Y
-onkel(X,Y):- maennlich(X),geschwister(eltern(Y,Z));(geschwister(eltern(Y,Z))).					% X ist Onkel von Y
-cousin(X,Y):- geschwister(eltern(Y,Z),eltern(X,Z)). 											% X ist Cousin von Y
-schwager(X,Y):- geschwister(Z,Y),ehemann(X,Z).													% X ist Schwager von Y
+% X ist Elternteil von Y
+eltern(X,Y):- vater(X,Y);mutter(X,Y).				
+% X ist Opa von Y
+opa(X,Y):- vater(X,Z),vater(Z,Y).
+% X ist Opa von Y
+oma(X,Y):- mutter(X,Z),mutter(Z,Y).
+% X und Y geschwister
+geschwister(X,Y):- vater(Z,X),vater(Z,Y),mutter(J,X),mutter(J,Y).
+% X und Y sind halbgeschwister
+halb_geschwister(X,Y):- vater(Z,X),vater(Z,Y),mutter(K,X),mutter(G,Y).
+% X und Y sind halbgeschister
+halb_geschwister(X,Y):- vater(Z,X),vater(J,Y),mutter(K,X),mutter(K,Y).
+% X ist schwester von
+schwester(X,Y):- weiblich(X),geschwister(X,Y).																	
+% X ist Tante von Y
+tante(X,Y):- weiblich(X),eltern(Z,Y),geschwister(Z,X).					
+% X ist Onkel von Y
+onkel(X,Y):- maennlich(X),eltern(Z,Y),geschwister(Z,X).
+% X ist Cousin von Y
+cousin(X,Y):- eltern(Z,Y),eltern(J,X),geschwister(Z,J). 											
+% X ist Schwager von Y
+schwager(X,Y):- geschwister(Z,Y),ehemann(X,Z).													
