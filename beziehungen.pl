e44d4b0235a34a4dacfc01c2f0e5aad7968b0ccf
bDino <buskulic.dino@gmail.com>
2014-04-02 09:12:31 +0200

    Rekursion immmer noch da, denke es liegt an der ehe beziehung

--- beziehungen.pl
+++ beziehungen.pl
@@ -43,7 +43,8 @@ vater(wolfgang,frauke).
 ehe(mia,rolf).
 ehe(eva,thomas).
 ehe(elsbeth,wolfgang).																				
-ehe(X,Y) :- ehe(Y,X).
+ehe(X,Y):- ehe(Y,X).
+
 % Beziehungen exemplarisch
 % X ist Elternteil von Y
 eltern(X,Y):- vater(X,Y);mutter(X,Y).				
@@ -60,13 +61,11 @@ halb_geschwister(X,Y):- vater(_,X),vater(_,Y),mutter(K,X),mutter(K,Y).
 % X ist schwester von
 schwester(X,Y):- weiblich(X),geschwister(X,Y).																	
 % X ist Tante von Y
-%tante(X,Y):- weiblich(X),eltern(Z,Y),not(eltern(X,Y)),geschwister(Z,X).	
-%tante(X,Y):- weiblich(X),eltern(Z,Y),geschwister(Z,X);ehe(X)
 tante(X,Y):- weiblich(X), ((eltern(Z, Y), geschwister(X, Z)) ; (eltern(Z, Y), geschwister(Z, C), ehe(X, C))), not(eltern(X,Y)).				
 % X ist Onkel von Y
-%onkel(X,Y):- maennlich(X),eltern(Z,Y),not(eltern(X,Y)),geschwister(Z,X).
-onkel(X,Y):- maennlich(X), (eltern(Z, Y), (geschwister(X, Z) ; (geschwister(Z, C), ehe(X, C)))), not(eltern(X,Y)).	
+onkel(X,Y):- maennlich(X), eltern(Z, Y), geschwister(X, Z), not(eltern(X,Y)).
+onkel(X,Y):- eltern(Z,Y),geschwister(Z, C),ehe(C,Z), not(eltern(X,Y)).	
 % X ist Cousin von Y
 cousin(X,Y):- eltern(Z,Y),eltern(J,X),geschwister(Z,J),not(geschwister(X,Y)).	 											
 % X ist Schwager von Y
-schwager(X,Y):- geschwister(Z,Y),ehemann(X,Z).													
+schwager(X,Y):- maennlich(X),geschwister(Z,Y),ehe(Z,X),not(ehe(X,Y)).													
