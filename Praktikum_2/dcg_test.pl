33729ea55d6be954466cba9f31023b565c46fb4e
bDino <buskulic.dino@gmail.com>
2014-04-24 14:04:58 +0200

    changes

--- Praktikum_2/dcg_test.pl
+++ Praktikum_2/dcg_test.pl
@@ -2,17 +2,17 @@
 :-consult(dcg).
 
 % Ergänzungsfragen
-test(egPhrase_ohne_art_tvj) :- satz(Sem,Strukt, [wer, ist, tante, von, jenzel], []).
-test(egPhrase_ohne_art_ovj) :- satz(Sem,Strukt, [wer, ist, onkel, von, jenzel], []).
-test(egPhrase_mit_art_tvj) :- satz(Sem,Strukt, [wer, ist, die, tante, von, jenzel], []).
-test(egphrase_mit_art_ovj) :- satz(Sem,Strukt, [wer, ist, onkel, von, jenzel], []).
+test(egPhrase_ohne_art_tvj) :- satz(Sem,Strukt, [wer, ist, tante, von, jenzel], []), Sem.
+test(egPhrase_ohne_art_ovj) :- satz(Sem,Strukt, [wer, ist, onkel, von, jenzel], []), Sem.
+test(egPhrase_mit_art_tvj) :- satz(Sem,Strukt, [wer, ist, die, tante, von, jenzel], []), Sem.
+test(egphrase_mit_art_ovj) :- satz(Sem,Strukt, [wer, ist, onkel, von, jenzel], []), Sem.
 
 % Entscheidungsfragen
-test(esPhrase_mit_art_tvj) :- satz(Sem,Strukt, [ist, thomas, der, onkel, von, jenzel], []).
-test(esPhrase_mit_art_ovf) :- satz(Sem,Strukt, [ist, thomas, der, onkel, von, frauke], []).
+test(esPhrase_mit_art_tvj) :- satz(Sem,Strukt, [ist, thomas, der, onkel, von, jenzel], []), Sem.
+test(esPhrase_mit_art_ovf) :- satz(Sem,Strukt, [ist, thomas, der, onkel, von, frauke], []), Sem.
 
 % FAIL
-test(esPhrase_ohne_art_tvjF, [fail]) :- satz(Sem,Strukt, [ist, thomas, der, onkel, von, elsbeth], []).
-test(esPhrase_ohne_art_ovjF, [fail]) :- satz(Sem,Strukt, [ist, thomas, der, onkel, von, elfriede], []).
-test(esPhrase_mit_art_tvjF, [fail]) :- satz(Sem,Strukt, [ist, thomas, der, onkel, von, thomas], []).
-test(esphrase_mit_art_ovjF, [fail]) :- satz(Sem,Strukt, [ist, thomas, der, onkel, von, ronja], []).
+test(esPhrase_ohne_art_tvjF, [fail]) :- satz(Sem,Strukt, [ist, thomas, der, onkel, von, elsbeth], []), Sem.
+test(esPhrase_ohne_art_ovjF, [fail]) :- satz(Sem,Strukt, [ist, thomas, der, onkel, von, elfriede], []), Sem.
+test(esPhrase_mit_art_tvjF, [fail]) :- satz(Sem,Strukt, [ist, thomas, der, onkel, von, thomas], []), Sem.
+test(esphrase_mit_art_ovjF, [fail]) :- satz(Sem,Strukt, [ist, thomas, der, onkel, von, ronja], []), Sem.
