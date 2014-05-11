6a6ad7f7322c3f8b7c5169bf93368dd2ee459857
Jan Menzel <menzel.jm@googlemail.com>
2014-05-11 15:26:56 +0200

    Working P2

--- Praktikum_2/dcg.pl
+++ Praktikum_2/dcg.pl
@@ -1,28 +1,44 @@
 :- load_files(['../Praktikum_1/beziehungen.pl', './lex.pl']).
-satz(SemS, struktur(IP, VP, PP)) -->  ip(IP, AGR),
-                                      vp(SemVp, VP, AGR),
-                                      pp(SemPP, PP, AGR),
-                                      {
-                                          SemS=..[SemVp,SemPP,_]
-                                      }.
-                                      
-satz(SemS, struktur(VP, PP)) -->      vp(SemVp, VP, AGR),
-                                      pp(SemPP, PP, AGR),
-                                      {
-                                          SemS=..[SemVp,SemPP,_]
-                                      }.
+%satz(SemS, struktur(IP, VP, PP)) -->  ip(IP, AGR),
+%                                      vp(SemVp, VP, AGR),
+%                                      pp(SemPP, PP, AGR),
+%                                      {
+%                                          SemS=..[SemVp,SemPP,_]
+%                                      }.
+%                                      
+%satz(SemS, struktur(VP, PP)) -->      vp(SemVp, VP, AGR),
+%                                      pp(SemPP, PP, AGR),
+%                                      {
+%                                          SemS=..[SemVp,SemPP,_]
+%                                      }.
 
-vp(SemVp, vp(V, NP), AGR)   --> v(V, AGR), np(SemVp, NP, AGR).
-vp(SemVp, vp(V, PN), AGR)   --> v(V, AGR), pn(PN, AGR), np(SemVp, NP, AGR).
-np(SemNp, np(Art, N), AGR)  --> det(Art, AGR), n(SemNp, N, AGR).
+% Wer ist die Tante von Emma?
+satz(SemS, struktur(IP, VP, PP)) --> ip(IP, AGR), 
+									 vp(SemVP, VP, AGR),
+									 pp(SemPP,PP, AGR),
+									 {
+									 	SemVP = [_,_,SemPP], 
+									 	SemS =.. SemVP
+									 }.	
+% Ist Rolf der Onkel von Emma?							
+satz(SemS, struktur(V, PN, NP, PP)) --> v(V, AGR), 
+										pn(SemPN, PN, AGR), 
+										np(SemNP, NP, AGR), 
+										pp(SemPP, PP, AGR),
+										{
+											SemS =.. [SemNP, SemPN, SemPP]
+										}.
 
 
+vp([SemNP, _, _], vp(V,NP), AGR) --> v(V, AGR), np(SemNP, NP, _).			% Verbalphrase
+np(SemN, np(DET, N), AGR) 		 --> det(DET, AGR), n(SemN, N, AGR).		% Nominalphrase
+np(SemN, np(N), AGR) 			 --> n(SemN, N, AGR).
+pp(SemPN, pp(PRAE, PN), AGR) 	 --> prae(PRAE, AGR), pn(SemPN, PN, AGR).	% Präpositionalphrase
 
-pp(_, pp(PN, PP), AGR) --> prae(PN, AGR), pn(PP, AGR).
-
-det(det(Art), AGR)      --> [X], {lex(X, Art, det, AGR)}.
-n(SemNp, n(SemNp), AGR) --> [X], {lex(X, SemNp, n, AGR)}.
-v(v(V), AGR)            --> [X], {lex(X, V, v, AGR)}.
-ip(ip(IP), AGR)         --> [X], {lex(X, IP, ip, AGR)}.
-pn(pn(PN), AGR)         --> [X], {lex(X, PN, pn, AGR)}.
-prae(prae(Prae), AGR)   --> [X], {lex(X, Prae, prae, AGR)}.
+% --- Terminale ---
+n(SemN, n(SemN), AGR) 			 --> [X], {lex(X, SemN, n, AGR)}.		% Nomen
+v(v(V), AGR) 					 --> [X], {lex(X, V, v, AGR)}.			% Verben
+det(det(DET), AGR) 				 --> [X], {lex(X, DET, det, AGR)}.		% Artikel
+ip(ip(IP), AGR) 				 --> [X], {lex(X, IP, ip, AGR)}.		% Interrogativpronomen
+prae(prae(PRAE), AGR) 			 --> [X], {lex(X, PRAE, prae, AGR)}.	% Präposition
+pn(SemPN, pn(SemPN), AGR) 		 --> [X], {lex(X, SemPN, pn, AGR)}.		% Eigenname
